db5af07a61185563f80b5aa12b790fc4b5c6c24d
ishrey4 <shreyasrinivas001@gmail.com>
2022-02-16 17:43:49 +0530

    first commit

---
+++ Shreya/src/javatpoint/simple.java
@@ -0,0 +1,11 @@
+package javatpoint;
+
+public class simple {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HELLO SHREYA WELCOME TO JAVA TRAINING");
+
+	}
+
+}
