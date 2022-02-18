4cb9f1948ca7585325e0603af6b3de9a0a557918
ishrey4 <shreyasrinivas001@gmail.com>
2022-02-18 12:09:44 +0530

    abstraction

---
+++ Abstract/HDFC.java
@@ -0,0 +1,8 @@
+package Abstract;
+
+public class HDFC extends Bank {
+	public void getrateofinterest() {
+		System.out.println("Hdfc bank rate of interest is low");
+	}
+
+}
---
+++ Abstract/overall.java
@@ -0,0 +1,14 @@
+package Abstract;
+
+public class overall  {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		sbi s=new sbi();
+		s.getrateofinterest();
+		HDFC h=new HDFC();
+		h.getrateofinterest();
+
+	}
+
+}
